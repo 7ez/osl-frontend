ae46149609fa105275be40a5b3140a7b55dd653d
Aochi <aochi@fuquila.net>
2023-09-11 01:01:31 +0200

    fix: escape '

--- src/app/setup/page.tsx
+++ src/app/setup/page.tsx
@@ -10,7 +10,7 @@ export default function Setup() {
                 <p className="text-xl">This is a tool that allows to manage osu! servers straight from your browser.</p>
                 <p className="text-xl">Setup is pretty simple and requires you to follow these steps:.</p>
                 <p className="text-xl">Download OSL Bridge (the app which launches your game/sets credentials) from <a href="https://github.com/7ez/osl-bridge/releases/latest/download/osl-bridge.exe" className="text-blue-400">here</a>.</p>
-                <p className="text-xl">Put the application in a safe path, which you won't delete by accident to ensure that it will always work. (ex. C:\Users\Aochi\osl)</p>
+                <p className="text-xl">Put the application in a safe path, which you won&apos;t delete by accident to ensure that it will always work. (ex. C:\Users\Aochi\osl)</p>
                 <p className="text-xl">After that, run the application as administrator and follow the instructions.<br/>Once completed, you can click <a href="/" className="text-blue-400">here</a> and start managing your osu! servers!</p>
             </div>
         </div>
