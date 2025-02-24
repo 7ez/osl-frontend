15f23d017cfdd9622b7fd59515d1120f6167cb12
Aochi <aochi@fuquila.net>
2025-02-24 19:48:12 +0100

    add: setup page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import AddServerDialog from "@/components/dialog/addserver";
 import Navbar from "@/components/navbar";
 import ServerList from "@/components/serverlist";
 import { Toaster } from "@/components/ui/toaster";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    if (localStorage.getItem("hasVisited") !== "true") {
+      localStorage.setItem("hasVisited", "true");
+      window.location.href = "/setup";
+    }
+  });
+
   return (
     <main className="h-screen">
       <Navbar />
